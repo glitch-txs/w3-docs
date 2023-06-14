3c6a55dfc8b675c869e6aa057d57051492ab2151
Glitch <glitch.txs@gmail.com>
2023-06-14 16:52:08 -0300

    remove glitch fx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ function HomepageHeader() {
           </Link>
         </div>
       </div>
-      <div className={styles.logo}><Glitch>Glitch.</Glitch></div>
+      <div className={styles.logo}>Glitch.</div>
     </header>
   );
 }
